6652d0c9b52449e87b5e2159fa0d23e77c1d5a68
havryliaksv <gavriliaks@gmail.com>
2023-03-16 11:06:30 +0200

    prepare project

---
+++ src/js/slider.js
@@ -0,0 +1,34 @@
+$(function () {
+  $('.клас слайдеру').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
