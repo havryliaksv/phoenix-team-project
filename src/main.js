c78e9231513c5573ad3d89defb29abce1d98a107
havryliaksv <gavriliaks@gmail.com>
2023-03-18 01:25:35 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import '/js/modal.js';
+import './js/modal.js';
 import 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js';
-import '/js/slick.min.js';
-import '/js/slider.js';
+import './js/slick.min.js';
+import './js/slider.js';
