85bcd936ab7ae0109028834a81140db8be31d6df
Insp1rer <igorkariymk@gmail.com>
2023-03-18 21:30:15 +0200

    Products upd 1.0

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,19 +1,63 @@
 $(function () {
   $('.reviews-list').slick({
     dots: true,
-    arrows: false,
     slidesToShow: 3,
     slidesToScroll: 1,
+    mobileFirst: true,
     responsive: [
       {
         breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
       {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
+$(function () {
+  $('.products-list').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
         breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          centerMode: true,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 375,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
